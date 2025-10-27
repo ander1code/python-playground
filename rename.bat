4ec4423f6067e80960ceb031ca6dc90924fd1b42
Anderson <andersonconceicao@protonmail.com>
2025-10-27 00:15:44 -0300

    folder sorting

---
+++ rename.bat
@@ -0,0 +1,17 @@
+@echo off
+
+ren "fundamentals" "1_fundamentals"
+ren "strings" "2_strings"
+ren "lambda" "3_lambda"
+ren "collections" "4_collections"
+ren "object oriented" "5_object_oriented"
+ren "database" "6_database"
+ren "network" "7_network"
+ren "sockets" "8_sockets"
+ren "threads" "9_threads"
+ren "frameworks" "10_frameworks"
+ren "courses" "11_courses"
+ren "advanced" "12_advanced"
+
+echo Renaming complete!
+pause
