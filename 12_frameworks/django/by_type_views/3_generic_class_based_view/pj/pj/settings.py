e26bd065c1449f5472080a68fd4f0e175d2095a5
Anderson <andersonconceicao@protonmail.com>
2026-01-13 02:45:16 -0300

    3_generic_class_based_view updated

--- 12_frameworks/django/by_type_views/3_generic_class_based_view/pj/pj/settings.py
+++ 12_frameworks/django/by_type_views/3_generic_class_based_view/pj/pj/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-swj6j2c+d4y*bj_gw(_$!luhyedpvugr&+%*xs_nr$q)qn+tl2'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
